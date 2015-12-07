29f344d7536d338752d3fa45257c6966d2aa0b99
suthaharan <webharan@gmail.com>
2015-12-07 02:29:42 -0500

    Committing .Net Changes

---
+++ Contact2015/Contact2015/Helpers/SessionValidAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contact2015.Helpers
+{
+    public class SessionValidAttribute
+    {
+/*
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string controllerName = filterContext.ActionDescriptor.ControllerDescriptor.ControllerName.ToLower();
+            if (!controllerName.Contains("home"))
+            {
+                HttpSessionStateBase session = filterContext.HttpContext.Session;
+                var user = session["ValidUserEmail"];
+                if (((user == null) && (!session.IsNewSession)) || (session.IsNewSession))
+                {
+                    //send them off to the login page
+                    var url = new UrlHelper(filterContext.RequestContext);
+                    var loginUrl = url.Content("~/Users/Login");
+                    filterContext.HttpContext.Response.Redirect(loginUrl, true);
+                }
+            }
+        }
+  */}
+}
